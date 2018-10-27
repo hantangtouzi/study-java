e1e26f62f81a7d6c57a9540715ad9a2a72f1014c
hantangtouzi <hantangtouzi@foxmail.com>
2018-10-27 16:11:36 +0800

    void

---
+++ Core/src/main/java/com/hantangtouzi/core/util/SystemUtil.java
@@ -0,0 +1,13 @@
+package com.hantangtouzi.core.util;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/10/27 16:10
+ */
+
+public class SystemUtil {
+    public static void main(String[] args) {
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+    }
+}
