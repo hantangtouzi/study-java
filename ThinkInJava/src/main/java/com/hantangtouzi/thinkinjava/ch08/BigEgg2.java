bd1058abf2bf485dfe84a51a4fbf19c6f080c3f9
hantangtouzi <hantangtouzi@foxmail.com>
2019-02-24 15:13:04 +0800

    void

---
+++ ThinkInJava/src/main/java/com/hantangtouzi/thinkinjava/ch08/BigEgg2.java
@@ -0,0 +1,60 @@
+package com.hantangtouzi.thinkinjava.ch08;
+
+/**
+ * @author WilliamChang.
+ * Created on 2019-02-24 14:44:51
+ */
+
+public class BigEgg2 extends Egg2 {
+    public class Yolk extends Egg2.Yolk {
+        public Yolk() {
+            System.out.println("BigEgg2.Yolk()");
+        }
+
+        public void f() {
+            System.out.println("BigEgg2.Yolk.f()");
+        }
+    }
+
+    public BigEgg2() {
+        insertYolk(new Yolk());
+    }
+
+    public static void main(String[] args) {
+        Egg2 e2 = new BigEgg2();
+        // e2.g();
+    }
+
+    // private int ppp = p();
+    //
+    // public int p () {
+    //     System.out.println("---------");
+    //     return 1;
+    // }
+}
+
+class Egg2 {
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg2.Yolk()");
+        }
+
+        public void f() {
+            System.out.println("Egg2.Yolk.f()");
+        }
+    }
+
+    private Yolk y = new Yolk();
+
+    public Egg2() {
+        System.out.println("New Egg2()");
+    }
+
+    public void insertYolk(Yolk yy) {
+        y = yy;
+    }
+
+    public void g() {
+        y.f();
+    }
+}
